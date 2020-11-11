81423d98890e8dc36b1ebea868839a47b1b91026
gillchristian <gillchristiang@gmail.com>
2020-11-11 22:35:31 +0100

    [WIP] Parser combinators implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,126 @@
-console.log("hi");
+import * as E from "fp-ts/Either"
+import {pipe} from "fp-ts/function"
+import * as P from "parser-ts/Parser"
+import * as C from "parser-ts/char"
+import * as S from "parser-ts/string"
+import {stream} from "parser-ts"
+
+interface LineComment {
+  type: "LineComment"
+  value: string
+}
+
+interface BlockComment {
+  type: "BlockComment"
+  value: string
+}
+
+interface StringLiteral {
+  type: "StringLiteral"
+  str: string
+}
+
+interface Char {
+  type: "Char"
+  char: string
+}
+
+type Comment = LineComment | BlockComment
+
+type Code = Char | StringLiteral | Comment
+
+const mkLineComment = (value: string): LineComment => ({type: "LineComment", value})
+const mkBlockComment = (value: string): BlockComment => ({type: "BlockComment", value})
+const mkChar = (char: string): Char => ({type: "Char", char})
+const mkStringLiteral = (str: string): StringLiteral => ({type: "StringLiteral", str})
+
+const isComment = (token: Code): token is Comment => token.type === "LineComment" || token.type === "BlockComment"
+
+// ---
+
+const singleQuotedString: P.Parser<string, String> = P.surroundedBy(C.char("'"))(
+  S.many(P.either(S.string("\\'"), () => C.notChar("'")))
+)
+
+const lineCommentStart = S.string("//")
+
+const blockCommentStart = S.string("/*")
+const blockCommentEnd = S.string("*/")
+
+const lineUntilEnd = P.manyTill(
+  P.sat((s: string) => s !== "\n"),
+  S.string("\n")
+)
+
+const lineComment = pipe(
+  P.seq(lineCommentStart, () => lineUntilEnd),
+  P.map((chars) => chars.join("")),
+  P.map(mkLineComment)
+)
+
+const blockComment = pipe(
+  P.seq(blockCommentStart, () =>
+    P.manyTill(
+      P.sat((_) => true),
+      blockCommentEnd
+    )
+  ),
+  P.map((chars) => chars.join("")),
+  P.map(mkBlockComment)
+)
+
+const anyChar = pipe(
+  P.sat<string>((_) => true),
+  P.map(mkChar)
+)
+
+const stringLit = pipe(
+  S.doubleQuotedString,
+  P.alt(() => singleQuotedString),
+  P.map(mkStringLiteral)
+)
+
+const commentOrChar: P.Parser<string, Code[]> = P.many(
+  pipe(
+    lineComment,
+    P.alt<string, Comment>(() => blockComment),
+    P.alt<string, Code>(() => stringLit),
+    P.alt<string, Code>(() => anyChar)
+  )
+)
+
+const onlyLines = pipe(
+  commentOrChar,
+  P.map((tokens) => tokens.filter(isComment))
+)
+
+const input = `contract Foo {}
+
+ // line comment
+
+// line comment  /* fasdfasd */
+
+/* /* block comment */
+
+/* block comment */
+contract Contract /* inlined block comment */ {
+    function foo(/* another inline comment */) public {
+        string s = "// \"double quotes";
+        string s2 = '// \'single quotes';
+    }
+}
+`
+
+pipe(
+  input.split(""),
+  stream.stream,
+  onlyLines,
+  E.fold(
+    (error) => console.log({error}),
+    (result) => console.log(result.value)
+  )
+)
+
+// Parser<string[]> => Parser<string> => Parser<LineComment>
+//
+// Parser<Comment[]>
---
+++ test/Contract.sol
@@ -0,0 +1,15 @@
+// line comment 
+
+// line comment  /* fasdfasd */
+
+/*
+
+  /* block comment */
+
+/* block comment */
+contract Contract /* inlined block comment */ {
+    function foo(/* another inline comment */) public {
+        string s = "// \"comment";
+        string s2 = '// \'comment';
+    }
+}
